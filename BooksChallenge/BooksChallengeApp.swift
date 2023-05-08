cb7ca55ee6d62125b84fffc66346a3c2d5e09168
Laurence Stone <larry.stone@gmail.com>
2023-05-08 07:20:35 -0500

    Initial Commit

---
+++ BooksChallenge/BooksChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksChallengeApp.swift
+//  BooksChallenge
+//
+//  Created by Laurence Stone on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct BooksChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
